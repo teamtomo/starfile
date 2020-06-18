2ebcaf9cd2fa057476871c9ea6352b2207e2e53a
Alister Burt <alisterburt@gmail.com>
2020-06-18 14:01:47 +0200

    add README.md add read and write functions in functions.py add necessary files for PyPI

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 from distutils.core import setup
 from starfile.version import VERSION
 setup(
-  name = 'starfile',         # How you named your package folder (MyLib)
-  packages = ['starfile'],   # Chose the same as "name"
-  version = f'{VERSION}',      # Start with a small number and increase it with every change you make
-  license='BSD 3-Clause License',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Read and write STAR files as pandas DataFrames',   # Give a short description about your library
-  author = 'AlisterBurt',                   # Type in your name
-  author_email = 'alisterburt@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/alisterburt/starfile',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['io', 'STAR'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  name = 'starfile',
+  packages = ['starfile'],
+  version = f'{VERSION}',
+  license='BSD 3-Clause License',
+  description = 'Read and write STAR files as pandas DataFrames',
+  author = 'Alister Burt',
+  author_email = 'alisterburt@gmail.com',
+  url = 'https://github.com/alisterburt/starfile',
+  download_url = 'https://github.com/alisterburt/starfile/archive/v01.tar.gz',
+  keywords = ['io', 'STAR', 'star', 'starfile'],
+  install_requires=[
           'pandas',
-          'openpyxl',
+          'openpyxl,
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: BSD 3-Clause License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD 3-Clause License',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
