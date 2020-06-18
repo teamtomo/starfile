024a7cab52f7222fc815d5a3f240d81d026a8a7a
Alister Burt <alisterburt@gmail.com>
2020-06-18 14:24:15 +0200

    fix license classifier

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
   classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
-    'License :: OSI Approved :: BSD 3-Clause License',
+    'License :: OSI Approved :: BSD License',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
