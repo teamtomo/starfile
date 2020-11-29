3ee5a1afcffd269c947a766f879e5f81c12131f8
Alister Burt <alisterburt@gmail.com>
2020-11-29 19:12:42 +0100

    writer is functional

--- starfile/writer.py
+++ starfile/writer.py
@@ -1,23 +1,28 @@
-import datetime
+from datetime import datetime
 from pathlib import Path
+from collections import OrderedDict
 from typing import Union, Dict, List
 
 import pandas as pd
 
+from .core import TextBuffer
 from .version import __version__
 
 
 class StarWriter:
     def __init__(self, dataframes: Union[pd.DataFrame, dict, list], filename: Union[Path, str],
-                 overwrite=False):
+                 overwrite=False, float_format='%.6f'):
         self.overwrite = overwrite
         self.filename = filename
         self.dataframes = dataframes
+        self.float_format = float_format
+        self.buffer = TextBuffer()
+        self.write_star_file()
 
     @property
     def dataframes(self):
         """
-        OrderedDict of pandas dataframes
+        Ordered dictionary of pandas dataframes
         df.name defines the data block name
         """
         return self._dataframes
@@ -33,14 +38,27 @@ class StarWriter:
         else:
             raise ValueError(f'Expected a DataFrame, Dict or List object, got {type(dataframes)}')
 
-    def coerce_dataframe(self, df: pd.DataFrame):
-        pass
+    @staticmethod
+    def coerce_dataframe(df: pd.DataFrame):
+        name = getattr(df, 'name', '')
+        if name != '':
+            name = 0
+        return {name: df}
 
-    def coerce_dict(self, dfs: Dict[pd.DataFrame]):
-        pass
+    @staticmethod
+    def coerce_dict(dfs: Dict[str, pd.DataFrame]):
+        """
+        This method ensures that dataframe names are updated based on dict keys
+        """
+        for key, df in dfs.items():
+            df.name = str(key)
+        return dfs
 
     def coerce_list(self, dfs: List[pd.DataFrame]):
-        pass
+        """
+        This method coerces a list of DataFrames into a dict
+        """
+        return self.coerce_dict(OrderedDict([(idx, df) for idx, df in enumerate(dfs)]))
 
     @property
     def filename(self):
@@ -48,7 +66,7 @@ class StarWriter:
 
     @filename.setter
     def filename(self, filename: Union[Path, str]):
-        self.filename = Path(filename)
+        self._filename = Path(filename)
         if not self.file_writable:
             raise FileExistsError('to overwrite an existing file set overwrite=True')
 
@@ -63,102 +81,61 @@ class StarWriter:
         else:
             return False
 
-    @property
-    def buffer(self):
-        return self._buffer
-
-    def clear_buffer(self):
-        self._buffer = ""
-
-    def add_line_to_buffer(self, line):
-        self._buffer += f'{line}\n'
-
-    def add_comment_to_buffer(self, line):
-        self.add_line_to_buffer(f'# {line}')
-
-    def add_blank_line_to_buffer(self):
-        self.add_line_to_buffer('')
-
-    def add_blank_lines_to_buffer(self, n: int):
-        for i in range(n):
-            self.add_blank_line_to_buffer()
-
-    def _write_buffer_to_disk(self):
-        with open(self.filename, 'a+') as file:
-            file.write(self.buffer)
-
-    def _write_buffer_and_clear(self):
-        self._write_buffer_to_disk()
-        self.clear_buffer()
-
-    def add_package_info_to_buffer(self):
-        now = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-        line = f'Created by the starfile Python package (version {__version__}) on {now}\n'
-        self.add_comment_to_buffer(line)
-
     def write_package_info(self):
-        self.add_package_info_to_buffer()
-        self._write_buffer_and_clear()
+        date = datetime.now().strftime('%d/%m/%Y')
+        time = datetime.now().strftime('%H:%M:%S')
+        line = f'Created by the starfile Python package (version {__version__}) at {time} on' \
+               f' {date}'
+        self.buffer.add_comment(line)
+        self.buffer.add_blank_lines(1)
+        self.buffer.write_as_new_file_and_clear(self.filename)
 
-    def write_star_file(self, filename: str = None, **kwargs):
+    def write_star_file(self, filename: str = None):
         self.write_package_info()
 
         for _, df in self.dataframes.items():
-            self._write_data_block(df, filename, **kwargs)
+            self.write_block(df)
 
-        self.add_blank_lines_to_buffer(2)
-        self._write_buffer_and_clear()
-        return
+        self.buffer.add_blank_line()
+        self.buffer.append_to_file_and_clear(self.filename)
 
-    def add_loopheader_to_buffer(self, df: pd.DataFrame):
-        self.add_line_to_buffer('loop_')
-        headings = [f'_{column_name} #{idx}' for idx, column_name in
-                    enumerate(df.columns, 1)]
-        for heading in headings:
-            self.add_line_to_buffer(heading)
+    def write_loopheader(self, df: pd.DataFrame):
+        self.buffer.add_line('loop_')
+        lines = [f'_{column_name} #{idx}' for idx, column_name in enumerate(df.columns, 1)]
 
-    def _write_loopheader(self, df: pd.DataFrame):
-        self.add_loopheader_to_buffer(df)
-        self._write_buffer_and_clear()
+        for line in lines:
+            self.buffer.add_line(line)
 
-    def get_block_name(self, df: pd.DataFrame):
-        return 'data_' + getattr(df, 'name')
+        self.buffer.append_to_file_and_clear(self.filename)
+
+    @staticmethod
+    def get_block_name(df: pd.DataFrame):
+        return 'data_' + getattr(df, 'name', '')
 
     def add_block_name_to_buffer(self, df: pd.DataFrame):
-        self.add_line_to_buffer(self.get_block_name(df))
+        self.buffer.add_line(self.get_block_name(df))
+        self.buffer.add_blank_lines(1)
+        self.buffer.append_to_file_and_clear(self.filename)
 
-    def _write_data_block(self, df: pd.DataFrame):
+    def write_block(self, df: pd.DataFrame):
         self.add_block_name_to_buffer(df)
 
         if df.shape[0] == 1:
             self._write_simple_block(df)
         elif df.shape[0] > 1:
             self._write_loop_block(df)
-        self._write_blank_lines( 2)
-        return
+        self.buffer.add_blank_lines(2)
+        self.buffer.append_to_file_and_clear(self.filename)
 
-    def _write_simple_block(self, dataframe: pd.DataFrame, filename: str):
-        entries = [f'_{column_name}\t\t\t{dataframe[column_name][0]}' for column_name in
-                   dataframe.columns]
+    def _write_simple_block(self, df: pd.DataFrame):
+        lines = [f'_{column_name}\t\t\t{df[column_name][0]}'
+                 for column_name in df.columns]
+        for line in lines:
+            self.buffer.add_line(line)
+        self.buffer.append_to_file_and_clear(self.filename)
 
-        with open(filename, 'a') as file:
-            for entry in entries:
-                file.write(f'{entry}\n')
-        return
+    def _write_loop_block(self, df: pd.DataFrame):
+        self.write_loopheader(df)
+        df.to_csv(self.filename, mode='a', sep='\t', header=False, index=False,
+                  float_format=self.float_format)
 
-    def _write_blank_lines(self, filename: str, n: int):
-        with open(filename, 'a') as file:
-            for i in range(n):
-                file.write('\n')
-        return
-
-    def _write_loop_block(self, dataframe: pd.DataFrame, filename: str, **kwargs):
-        self._write_loopheader(dataframe, filename)
-
-        # new main block
-        if 'float_format' not in kwargs.keys():
-            dataframe.to_csv(filename, mode='a', sep='\t', header=False, index=False,
-                             float_format='%.8f', **kwargs)
-        else:
-            dataframe.to_csv(filename, mode='a', sep='\t', header=False, index=False, **kwargs)
-        return
