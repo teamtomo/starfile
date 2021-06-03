b3f860a309c26b4c703be1435ef624932991f283
Lorenzo Gaifas <brisvag@gmail.com>
2021-06-03 16:10:31 +0200

    migrated to setup.cfg and made cli optional

--- setup.py
+++ setup.py
@@ -1,35 +1,3 @@
 from setuptools import setup
-from starfile.version import __version__
 
-setup(
-    name='starfile',
-    version=__version__,
-    packages=['starfile'],
-    url='https://github.com/alisterburt/starfile',
-    license='BSD 3-Clause License',
-    author='Alister Burt',
-    author_email='alisterburt@gmail.com',
-    description='STAR file reading and writing in python',
-    python_requires='>=3.6',
-    install_requires=[
-        'pandas',
-        'Click',
-        'IPython',
-    ],
-    py_modules=['starfile'],
-    entry_points={
-        'console_scripts': [
-            'starfile = starfile.__main__:cli',
-        ],
-    },
-    classifiers=[
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
-)
+setup()
--- starfile/__main__.py
+++ starfile/__main__.py
@@ -1,26 +1,32 @@
-import sys
+try:
+    from IPython.terminal.embed import InteractiveShellEmbed
+    import click
+except ImportError:
+    deps = False
+else:
+    deps = True
 
-from IPython.terminal.embed import InteractiveShellEmbed
-from traitlets.config.loader import Config
-import click
 
-from .functions import read, write
+if deps:
+    from .functions import read, write
 
+    @click.command()
+    @click.argument('path', type=click.Path(exists=True, dir_okay=False, readable=True))
+    @click.option('--read_n_blocks', type=int)
+    @click.option('--always_dict', is_flag=True)
+    def cli(path, read_n_blocks, always_dict):
+        """
+        Read a star file and open an ipython console to interactively inspect its contents
+        """
+        star = read(path, read_n_blocks, always_dict)
 
-@click.command()
-@click.argument('path', type=click.Path(exists=True, dir_okay=False, readable=True))
-@click.option('--read_n_blocks', type=int)
-@click.option('--always_dict', is_flag=True)
-def cli(path, read_n_blocks, always_dict):
-    """
-    Read a star file and open an ipython console to interactively inspect its contents
-    """
-    star = read(path, read_n_blocks, always_dict)
-
-    banner = '''=== Starfile ===
-- access your data with `star`
-- write it out with `write(...)`
-    '''
-    sh = InteractiveShellEmbed(banner2=banner)
-    sh.push('star')
-    sh()
+        banner = '''=== Starfile ===
+    - access your data with `star`
+    - write it out with `write(...)`
+        '''
+        sh = InteractiveShellEmbed(banner2=banner)
+        sh.push('star')
+        sh()
+else:
+    def cli():
+        print('To use the command line utility, install with `pip install starfile[cli]`')
