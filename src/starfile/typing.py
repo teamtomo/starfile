cea36b53d46fd451cf608dd1b9ffda9946f2c543
Dan Hatton <daniel.hatton@diamond.ac.uk>
2024-03-17 03:16:12 +0000

    DataBlocks may not contain Polars DataFrames rather than Pandas

--- src/starfile/typing.py
+++ src/starfile/typing.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
-from typing import Union, Dict
-from typing_extensions import TypeAlias
+from typing import Dict, Union
 
 import pandas as pd
+import polars as pl
+from typing_extensions import TypeAlias
 
 DataBlock: TypeAlias = Union[
-    pd.DataFrame,
-    Dict[str, Union[str, int, float]]
+    pd.DataFrame | pl.DataFrame, Dict[str, Union[str, int, float]]
 ]
